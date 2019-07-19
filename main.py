f9b53dc23a6a069b96af63f8d0a9d7c81ba484b9
mahboobehad <mhstar32@gmail.com>
2019-07-19 12:59:09 +0430

    feat: added main.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # In The name of God
+from telegram import Bot
+from telegram.ext import Updater
+
+from config import Config
+from controller.start_controller import StartController
 
 if __name__ == '__main__':
-    pass
+    bot = Bot(token=Config.bot_token, base_url=Config.base_url, base_file_url=Config.base_file_url)
+    updater = Updater(bot=bot)
+    dispatcher = updater.dispatcher
+    StartController(dispatcher, bot)
+    updater.start_polling()
