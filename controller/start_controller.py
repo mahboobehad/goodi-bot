399119de5f2d67e168a10f454bf48a90c48b7d15
mahboobehad <mhstar32@gmail.com>
2019-07-19 17:22:45 +0430

    feat: added user_data to controller.

--- controller/start_controller.py
+++ controller/start_controller.py
@@ -2,6 +2,7 @@
 from telegram import Bot, Update
 from telegram.ext import ConversationHandler, CommandHandler, RegexHandler, Dispatcher
 
+from view.constant_messages import Keyboard
 from view.start_view import StartView
 
 
@@ -11,12 +12,19 @@ class StartController:
         self.dispatcher = dispatcher
         self.conversation_handler = ConversationHandler(
             entry_points=[CommandHandler("start", self.start),
-                          RegexHandler(pattern="منوی اصلی", callback=self.main_menu)
+                          RegexHandler(pattern=Keyboard.main_menu, callback=self.main_menu)
                           ],
             states={},
             fallbacks={}
         )
         self.dispatcher.add_handler(self.conversation_handler)
+        self.user_data = {}
+
+    def add_user_data(self, user_id, key, value):
+        if not user_id in self.user_data.keys():
+            self.user_data[user_id] = {key: value}
+        else:
+            self.user_data[user_id].update({key: value})
 
     def start(self, bot: Bot, update: Update):
         chat_id = update.effective_chat.id
